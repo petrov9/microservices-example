beeec1e0cd2c1a4a343cdd6ed3db9845cf4de556
Anton Petrov <petrov9@inbox.ru>
2021-02-17 11:52:02 +0300

    Parse field from JWT

---
+++ gateway/src/main/java/org/example/gateway/filter/TrackingFilter.java
@@ -0,0 +1,42 @@
+package org.example.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONObject;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Order(1)
+@Slf4j
+public class TrackingFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+
+        String token = headers.get("Authorization").stream().findFirst().get();
+        JSONObject jsonObject = decodeJWT(token);
+
+        String secretField = jsonObject.getString("secret_field");
+
+        log.info("Decoded secret_field: " + secretField);
+
+        return chain.filter(exchange);
+    }
+
+    private JSONObject decodeJWT(String JWTToken) {
+        String[] split_string = JWTToken.split("\\.");
+        String base64EncodedBody = split_string[1];
+        Base64 base64Url = new Base64(true);
+        String body = new String(base64Url.decode(base64EncodedBody));
+        JSONObject jsonObj = new JSONObject(body);
+        return jsonObj;
+    }
+
+}
